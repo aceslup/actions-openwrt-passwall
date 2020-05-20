e56e7e0799bd54e4c501646fd3166a1582e1b23c
Aceslup <aceslup@users.noreply.github.com>
2020-05-20 20:57:52 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,4 +6,6 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.123.99/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.0.254/g' package/base-files/files/bin/config_generate
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
